229c7443e980c8034924776687c3f2cd30794f09
Cooper Saye <coopersaye@gmail.com>
2020-09-16 18:58:58 -0700

    Implement board intialization.

--- game.cpp
+++ game.cpp
@@ -1,11 +1,12 @@
+#include <vector>
 #include <thread>
 
 #include "game.h"
 #include "renderer.h"
 #include "renderer.cpp"
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 600;
 const int FPS = 10;
 
 Game::Game()
@@ -18,13 +19,24 @@ void Game::gameOver()
     running = false;
 }
 
+void Game::initializeBoard()
+{
+    for (int col = 0; col < SCREEN_WIDTH; col += 100)
+    {
+        std::vector<int> column;
+        for (int row = 0; row < SCREEN_HEIGHT; row += 100) column.push_back(0);
+        board.push_back(column);
+    }
+}
+
 void Game::startGameLoop()
 {
-    running = true;
-    
     Renderer renderer(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    running = true;
     SDL_Event event;
+    Direction direction;
+    initializeBoard();
 
     while (running)
     {
@@ -33,10 +45,13 @@ void Game::startGameLoop()
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT) gameOver();
-            if (event.type == SDL_KEYDOWN) gameOver();
-            if (event.type == SDL_MOUSEBUTTONDOWN) gameOver();
+
+            if (event.type == SDLK_UP) direction = Direction::up;
+            if (event.type == SDLK_DOWN) direction = Direction::down;
+            if (event.type == SDLK_LEFT) direction = Direction::left;
+            if (event.type == SDLK_RIGHT) direction = Direction::right;
         }
-        
+
         renderer.render();
     }
 }
--- game.h
+++ game.h
@@ -1,14 +1,19 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <vector>
+
 class Game
 {
     public:
         Game();
         void gameOver();
+        enum class Direction { up, down, left, right };
     private:
+        void initializeBoard();
         void startGameLoop();
         bool running;
+        std::vector<std::vector<int>> board;
 };
 
 #endif
